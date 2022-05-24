3c6c8d29a0329774b1b8dd46a84f537f2d97d02d
Quick <quick@arkyn.tech>
2022-05-24 13:44:22 -0400

    Update poster_overlay.py

--- poster_overlay.py
+++ poster_overlay.py
@@ -19,7 +19,7 @@ from datetime import datetime
 # Do not edit these, use the config file to make any changes
 
 config_object = ConfigParser()
-config_object.read("/config/config.ini")
+config_object.read("config.ini")
 server = config_object["PLEXSERVER"]
 baseurl = (server["PLEX_URL"])
 token = (server["TOKEN"])
@@ -30,63 +30,40 @@ pbak = str.lower((server["POSTER_BU"]))
 HDR_BANNER = str.lower((server["HDR_BANNER"]))
 plex = PlexServer(baseurl, token)
 films = plex.library.section(plexlibrary)
-mini_4k = str.lower((server["mini_4k"]))
+television = plex.library.section(plextvlibrary)
+televisiondv = plex.library.section(plextvdvlibrary)
+dvfilms = plex.library.section(plexdvlibrary)
 banner_4k = Image.open("img/4K-Template.png")
-mini_4k_banner = Image.open("img/4K-mini-Template.png")
-banner_hdr = Image.open("img/hdr-poster.png")
-chk_banner = Image.open("img/chk-4k.png")
-chk_mini_banner = Image.open("img/chk-mini-4k.png")
-chk_hdr = Image.open("img/chk_hdr.png")
-size = (911,1367)
-box= (0,0,911,100)
-mini_box = (0,0,301,268)
-hdr_box = (0,611,215,720)
+banner_4k_hdr = Image.open("4k-hdr-poster.png")
+banner_4k_dv = Image.open("4k-dv-poster.png")
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print(current_time, ": 4k HDR poster script starting now.")          
 
-def add_banner():
+def add_banner4k():
     background = Image.open('poster.png')
-    background = background.resize(size,Image.ANTIALIAS)
+    background = background.resize(size,Image.Resampling.LANCZOS)
     backgroundchk = background.crop(box)
-    hash0 = imagehash.average_hash(backgroundchk)
-    hash1 = imagehash.average_hash(chk_banner)
-    cutoff= 5
-    if hash0 - hash1 < cutoff:
-        print('4K banner exists, moving on...')
-    else:
-        background.paste(banner_4k, (0, 0), banner_4k)
-        background.save('poster.png')
-        i.uploadPoster(filepath="poster.png")
+    background.paste(banner_4k, (0, 0), banner_4k)
+    background.save('poster.png')
+    i.uploadPoster(filepath="poster.png")
 
-def add_mini_banner():
+def add_banner4khdr():
     background = Image.open('poster.png')
-    background = background.resize(size,Image.ANTIALIAS)
-    backgroundchk = background.crop(mini_box)
-    hash0 = imagehash.average_hash(backgroundchk)
-    hash1 = imagehash.average_hash(chk_mini_banner)
-    cutoff= 15
-    if hash0 - hash1 < cutoff:
-        print('4K banner exists, moving on...')
-    else:
-        background.paste(mini_4k_banner, (0, 0), mini_4k_banner)
-        background.save('poster.png')
-        i.uploadPoster(filepath="poster.png")
+    background = background.resize(size,Image.Resampling.LANCZOS)
+    backgroundchk = background.crop(box)
+    background.paste(banner_4khdr, (0, 0), banner_4khdr)
+    background.save('poster.png')
+    i.uploadPoster(filepath="poster.png")
     
-def add_hdr():
+def add_banner4kdv():
     background = Image.open('poster.png')
-    background = background.resize(size,Image.ANTIALIAS)
-    backgroundchk = background.crop(hdr_box)
-    hash0 = imagehash.average_hash(backgroundchk)
-    hash1 = imagehash.average_hash(chk_hdr)
-    cutoff= 5
-    if hash0 - hash1 < cutoff:
-        print('HDR banner exists, moving on...')
-    else:
-        background.paste(banner_hdr, (0, 0), banner_hdr)
-        background.save('poster.png')
-        i.uploadPoster(filepath="poster.png")
+    background = background.resize(size,Image.Resampling.LANCZOS)
+    backgroundchk = background.crop(box)
+    background.paste(banner_4kdv, (0, 0), banner_4kdv)
+    background.save('poster.png')
+    i.uploadPoster(filepath="poster.png")    
 
 def get_poster():
     newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+'/'
@@ -109,56 +86,36 @@ def get_poster():
         print(Fore.RED+films.title+"cannot find the poster for this film")
         print(Fore.RESET)
 
+def poster_4k_dv():
+    print(i.title + ' 4k DV')     
+    get_poster()
+    add_bannerdv()                                  
+    os.remove('poster.png') 
+
 def poster_4k_hdr():
     print(i.title + ' 4k HDR')     
     get_poster()
-    add_banner() 
-    add_hdr()                                  
+    add_bannerhdr()                                  
     os.remove('poster.png')              
 
 def poster_4k():   
     print(i.title + " 4K Poster")
     get_poster()
-    if mini_4k == 'true':
-        add_mini_banner()
-    else:
-        add_banner()                                  
+    add_banner4k()                                  
     os.remove('poster.png')   
-                  
-def poster_hdr():
-    print(i.title + " HDR Poster") 
-    get_poster() 
-    add_hdr()                                  
-    os.remove('poster.png')              
-
-if HDR_BANNER == 'true':
-    for i in films.search(resolution="4k", hdr=False):
-        try:
-            poster_4k()
-        except FileNotFoundError:
-            print(Fore.RED+films.title+" Error, the 4k poster for this film could not be created.")
-            print(Fore.RESET)
-            continue    
-    for i in films.search(resolution="4k", hdr=True):
-        try:
-            poster_4k_hdr()
-        except FileNotFoundError:
-            print(Fore.RED+films.title+" Error, the 4k HDR poster for this film could not be created.")
-            print(Fore.RESET)
-            continue
-    for i in films.search(resolution="1080,720", hdr=True):
-        try:
-            poster_hdr()
-        except FileNotFoundError:
-            print(Fore.RED+films.title+" Error, the HDR poster for this film could not be created.")
-            print(Fore.RESET)
-            continue
-else:
-    print('Creating 4k posters only')
-    for i in films.search(resolution="4k"):
-        try:
-            poster_4k()
-        except FileNotFoundError:
-            print(Fore.RED+films.title+" Error, the 4k poster for this film could not be created.")
-            print(Fore.RESET)
-            continue
+    
+            
+for i in films.search(**{"hdr": False, "label!": "Overlay"}):
+    try:
+        poster_4k()
+    except FileNotFoundError:
+        print(Fore.RED+films.title+" Error, the 4k poster for this film could not be created.")
+        print(Fore.RESET)
+        continue    
+for i in films.search(**{"hdr": True, "label!": "Overlay"}):
+    try:
+        poster_4k_hdr()
+    except FileNotFoundError:
+        print(Fore.RED+films.title+" Error, the 4k HDR poster for this film could not be created.")
+        print(Fore.RESET)
+        continue
