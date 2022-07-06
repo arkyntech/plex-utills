d462aabbca1e6cb9f678ca3ac5daa5c1c75de258
Quick <quick@arkyn.tech>
2022-07-06 14:48:04 -0400

    Update 4k_hdr_poster.py

--- 4k_hdr_poster.py
+++ 4k_hdr_poster.py
@@ -31,6 +31,9 @@ dvfilms = plex.library.section(plexdvlibrary)
 banner_4k = Image.open("4K-Template.png")
 banner_4k_hdr = Image.open("4k-hdr-poster.png")
 banner_4k_dv = Image.open("4k-dv-poster.png")
+banner_4k_imax = Image.open("imax-poster.png")
+banner_4k_imaxhdr = Image.open("imax-hdr-poster.png")
+banner_4k_imaxdv = Image.open("imax-dv-poster.png")
 size = (911,1367)
  
 
@@ -99,6 +102,105 @@ def poster_4k_dv():
     i.uploadPoster(filepath="poster.png")
     i.addLabel("Overlay")
     os.remove('poster.png') 
+
+def poster_4k_imax():
+    print(i.title + ' 4k DV')    
+    if platform.system() == 'Windows':
+        newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+"\\" 
+    else:
+        newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+'/'
+    backup = os.path.exists(newdir+'poster_bak.png')  
+    imgurl = i.posterUrl
+    img = requests.get(imgurl, stream=True)
+    filename = "poster.png"
+    if pbak == True: 
+        if backup == True: 
+            print('Backup File Exists, Skipping...')    
+        else:
+            print('Creating a backup file')
+            dest = shutil.copyfile(filename, newdir+'poster_bak.png')
+            os.chown(newdir+'poster_bak.png', 99, 100)
+            os.chmod(newdir+'poster_bak.png', 0o0666)
+
+    if img.status_code == 200:
+        img.raw.decode_content = True
+        with open(filename, 'wb') as f:
+            shutil.copyfileobj(img.raw, f)
+
+    print('creating poster')    
+    background = Image.open('poster.png')
+    background = background.resize(size,Image.Resampling.LANCZOS)
+    background.paste(banner_4k_imax, (0, 0), banner_4k_imax)
+    background.save('poster.png')
+    i.uploadPoster(filepath="poster.png")
+    i.addLabel("Overlay")
+    os.remove('poster.png'
+
+def poster_4k_imaxhdr():
+    print(i.title + ' 4k DV')    
+    if platform.system() == 'Windows':
+        newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+"\\" 
+    else:
+        newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+'/'
+    backup = os.path.exists(newdir+'poster_bak.png')  
+    imgurl = i.posterUrl
+    img = requests.get(imgurl, stream=True)
+    filename = "poster.png"
+    if pbak == True: 
+        if backup == True: 
+            print('Backup File Exists, Skipping...')    
+        else:
+            print('Creating a backup file')
+            dest = shutil.copyfile(filename, newdir+'poster_bak.png')
+            os.chown(newdir+'poster_bak.png', 99, 100)
+            os.chmod(newdir+'poster_bak.png', 0o0666)
+
+    if img.status_code == 200:
+        img.raw.decode_content = True
+        with open(filename, 'wb') as f:
+            shutil.copyfileobj(img.raw, f)
+
+    print('creating poster')    
+    background = Image.open('poster.png')
+    background = background.resize(size,Image.Resampling.LANCZOS)
+    background.paste(banner_4k_imaxhdr, (0, 0), banner_4k_imaxhdr)
+    background.save('poster.png')
+    i.uploadPoster(filepath="poster.png")
+    i.addLabel("Overlay")
+    os.remove('poster.png'
+
+def poster_4k_imaxdv():
+    print(i.title + ' 4k DV')    
+    if platform.system() == 'Windows':
+        newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+"\\" 
+    else:
+        newdir = os.path.dirname(re.sub(ppath, mpath, i.media[0].parts[0].file))+'/'
+    backup = os.path.exists(newdir+'poster_bak.png')  
+    imgurl = i.posterUrl
+    img = requests.get(imgurl, stream=True)
+    filename = "poster.png"
+    if pbak == True: 
+        if backup == True: 
+            print('Backup File Exists, Skipping...')    
+        else:
+            print('Creating a backup file')
+            dest = shutil.copyfile(filename, newdir+'poster_bak.png')
+            os.chown(newdir+'poster_bak.png', 99, 100)
+            os.chmod(newdir+'poster_bak.png', 0o0666)
+
+    if img.status_code == 200:
+        img.raw.decode_content = True
+        with open(filename, 'wb') as f:
+            shutil.copyfileobj(img.raw, f)
+
+    print('creating poster')    
+    background = Image.open('poster.png')
+    background = background.resize(size,Image.Resampling.LANCZOS)
+    background.paste(banner_4k_imaxdv, (0, 0), banner_4k_imaxdv)
+    background.save('poster.png')
+    i.uploadPoster(filepath="poster.png")
+    i.addLabel("Overlay")
+    os.remove('poster.png'
 
 def poster_4k():   
     print(i.title + " 4K Poster")
@@ -203,3 +305,13 @@ for i in television.search(**{"hdr": True, "label!": "Overlay"}):
     poster_4ktvhdr()
 for i in televisiondv.search(**{"label!": "Overlay"}):
     poster_4ktvdv()    
+for i in imaxhdrfilms.search(Media__Part__file__contains="IMAX",**{"hdr": True, "label!": "Overlay"}):
+    poster_4k_imaxhdr()
+for i in imaxdvfilms.search(Media__Part__file__contains="IMAX",**{"hdr": False, "resolution": "4k", "label!": "Overlay"}):
+    poster_4k_imaxdv()
+for i in imaxfilms.search(Media__Part__file__contains="IMAX",**{"hdr": False, "resolution": "1080", "label!": "Overlay"}):
+    poster_imax()
+for i in imaxfilms.search(Media__Part__file__icontains="dv.hevc",**{"label!": "Overlay"}):
+    poster_4k_dv()
+for i in imaxfilms.search(Media__Part__file__icontains="hdr.hevc",**{"hdr": True, "label!": "Overlay"}):
+    poster_4k_hdr()
